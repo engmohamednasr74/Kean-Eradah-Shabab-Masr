c8cf993c821732917b2830c1560b09c57d813bef
engmohamednasr74 <eng.mohamed.nasr.74@gmail.com>
2026-02-24 16:29:06 +0200

    وصف التعديل

--- js/gov.js
+++ js/gov.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
         targetUrl = "../../events.html"; // ← أو "./events.html" حسب الموقع
       } else {
         // للمحافظات → مسار نسبي صحيح
-        targetUrl = `Kean-Eradah-Shabab-Masr/gov/${slug}/events.html`;
+        targetUrl = `gov/${slug}/events.html`;
         // اضف اسم كيان قبل رفع الموقع
         // Kean-Eradah-Shabab-Masr
       }
